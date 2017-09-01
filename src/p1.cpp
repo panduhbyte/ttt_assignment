ff6c5be7cad8da23e94343fd6f98e4fea2a4f9fb
Charles Valdez <cvldz03@gmail.com>
2017-08-31 23:47:20 -0400

    Game almost done.
    Did major changes. Forgot to commit and push. Basically the last thing to do is catch the error of using an already used box.

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,26 +1,8 @@
-/*
-In this project, you will implement the tic-tac-toe game for a 3 by 3 square.
-The game will be played by two players (players A and B).
-All output are to the console. Follow these guidelines:
 
-1)	Draw the board (ensure its nicely formatted as a square)
-	after each move and make sure to prompt the user with enough information on what to input;
-	if the user tries to use an occupied space, then he effectively loses his move (no redoes!);
-	any illegal characters input also results in loss of the move (your implementation will check for all this).
-
-2)	You are given the outline of the program that you can use to start the project.
-	Make sure you test your implementation well against boundary cases, illegal input, etc.
-
-3)	Turn in your source file to Canvas by the deadline.
-	There will be no extensions or late credit.
- */
 #include <iostream>
 using namespace std;
 
-char board[9] = {'1', '2', '3',	// Basic board.
-				'4', '5', '6',
-				'7', '8', '9'};
-int player;
+char board[9];
 
 void draw(){
 	cout << "[ "<< board[0] << " ]";
@@ -33,22 +15,6 @@ void draw(){
 	cout << "[ "<< board[7] << " ]";
 	cout << "[ "<< board[8] << " ]\n"; // End of 1st row.
 }
-
-void play(int player){
-			// this function contains  the game logic
-			// assume a move by player A, followed by player B
-			// after each move, you must check if the respective player
-			// has won, in which case the game is complete; use a separate
-			// function to accomplish this.
-			// players will enter the position where they want to enter in
-			// the tic-tac-toe square. Assume the numbering from 1 to 9 
-			// in row-major order 
-			// use cin and cout for input and output
-
-}
-
-// This function determines if the game has ended - either a row, column or
-// a diagonal should be filled with the same character for it to be complete.
 
 bool check(){
 	bool r;
@@ -68,8 +34,75 @@ bool check(){
 	return r;
 }
 
+void play(int player){
+	int box;
+
+	char xo;
+	if(player == 1) xo = 'X';
+	else xo = 'O';
+
+	draw();
+
+	cout << "Player " << player << " enter a number 1-9.\n";
+	if( !(cin >> box) ){
+		cin.clear();
+		cin.ignore();
+		cout << "Your entry is invalid and turn is skipped.\n";
+	}
+	else{
+		box = box - 1;
+		board[box] = xo;
+	}
+}
+
 
 int main(){
 
+	board[0] = '1';
+	board[1] = '2';
+	board[2] = '3';
+	board[3] = '4';
+	board[4] = '5';
+	board[5] = '6';
+	board[6] = '7';
+	board[7] = '8';
+	board[8] = '9';
+
+	int player = 1;
+	int i = 0;
+	bool end_game = false;
+
+	do{
+		play(player);
+		end_game = check();
+		i = i + 1;
+
+		if(end_game == true){
+			cout << "Player " << player << " has won.\n";
+			draw();
+			return 0;
+		}
+		else if(player == 1) player = 2;
+		else player = 1;
+	}while(i < 9);
+
+	if (end_game == false) cout << "Cat's game.\n";
+	draw();
 	return 0;
 }
+/*
+In this project, you will implement the tic-tac-toe game for a 3 by 3 square.
+The game will be played by two players (players A and B).
+All output are to the console. Follow these guidelines:
+
+1)	Draw the board (ensure its nicely formatted as a square)
+	after each move and make sure to prompt the user with enough information on what to input;
+	if the user tries to use an occupied space, then he effectively loses his move (no redoes!);
+	any illegal characters input also results in loss of the move (your implementation will check for all this).
+
+2)	You are given the outline of the program that you can use to start the project.
+	Make sure you test your implementation well against boundary cases, illegal input, etc.
+
+3)	Turn in your source file to Canvas by the deadline.
+	There will be no extensions or late credit.
+ */
